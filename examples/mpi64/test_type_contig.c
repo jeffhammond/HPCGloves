690e45baeb16430a782aeb9b34642761016e942b
Jeff Hammond <jeff.science@gmail.com>
2013-10-13 15:07:48 -0500

    preparing test

--- examples/mpi64/test_type_contig.c
+++ examples/mpi64/test_type_contig.c
@@ -14,15 +14,22 @@
 #include <mpi.h>
 #include "bigmpix.h"
 
-#define BIGMPI_MAX 2147483648
-
 // int MPIX_Type_contiguous_x(MPI_Count count, MPI_Datatype oldtype, MPI_Datatype * newtype);
 
 int main(int argc, char * argv[])
 {
+    int provided;
+    MPI_Init_thread(&argc, &argv, MPI_THREAD_SINGLE, &provided);
 
+    int rank, size;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
 
+    int logn = (argc>1) ? atoi(argv[1]) : 32;
+    MPI_Count count = (MPI_Count)1<<logn; /* explicit cast required */
+    printf("count = %lld \n", count);
 
+    MPI_Finalize();
 
     return 0;
 }
